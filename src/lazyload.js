5bdb5349f98a7b000db82fa8f21239ebc7703f9d
bobsam813 <bobsam@bobsamdeMacBook-Pro.local>
2016-12-26 17:00:16 +0800

    提交文件

---
+++ src/lazyload.js
@@ -0,0 +1,14 @@
+(function(window, document, undefined) {
+    var WINDOW_TOP = 0;
+    var SCROLL_TOP = 0;
+    var aImg = document.getElementsByTagName('img');
+    var LazyLoad = {
+        init: fInit
+    };
+
+    
+
+    function fInit(config){
+        
+    }
+})(window, document);
